46bbb871d981036e60b12c1dbae7e266ab33a835
Bruce Harris <bharris845@gmail.com>
2014-11-11 17:44:07 -0500

    added instructions

--- functional-iteration/index.js
+++ functional-iteration/index.js
@@ -2,13 +2,23 @@
 
 var numbers = [3, 4, 9];
 
+function square(x) {
+  return x * x;
+}
+
+
+// re-implement the below using numbers.map(...)
+
 var numbersSquared = [];
 
 for (var i = 0; i < numbers.length; i++) {
   numbersSquared.push(numbers[i] * numbers[i]);
 }
 
+//////////////////////////////////////////////
+
 module.exports = {
   numbers: numbers,
-  numbersSquared: numbersSquared
+  numbersSquared: numbersSquared,
+  square: square
 };
--- functional-iteration/test.js
+++ functional-iteration/test.js
@@ -3,13 +3,10 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-function square(x) {
-  return x * x;
-}
-
 describe('squaring numbers in an array', function() {
   var numbersSquared = require('./index').numbersSquared;
   var numbers = require('./index').numbers;
+  var square = require('./index').square;
 
   it('should contain expected values', function() {
     expect(numbersSquared[0]).to.equal(square(numbers[0]));
